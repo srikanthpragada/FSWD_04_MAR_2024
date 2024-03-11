a14fa7cfc5c9ad7f7605298e59ca9442329cb764
srikanthpragada <srikanthpragada@gmail.com>
2024-03-11 20:55:27 +0530

    11-MAR

---
+++ js/hoisting.js
@@ -0,0 +1,6 @@
+console.log(a)
+
+var a = 100
+
+console.log(a)
+
---
+++ js/operators.js
@@ -0,0 +1,11 @@
+let a = 10
+let b = "10"
+
+console.log(a == b, a === b)
+
+n1 = 10
+n2 = 15
+
+console.log(  n1 > n2 ? n1 : n2)
+
+console.log(typeof(b))
